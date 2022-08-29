571af5a54a28c91e5e6ef576d949f6c80bd91ee1
Frederico <frederico2947@gmail.com>
2022-08-29 16:33:44 +0700

    edit php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo ucwords(str_replace('_','',$page)) ?> | Simple Online Groceries Ordering System</title>
 </head>
 <body>
     
