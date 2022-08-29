533d804fba605d3ba6a6e5de6177eceadbf149ca
Frederico <frederico2947@gmail.com>
2022-08-29 16:34:13 +0700

    edit php

--- index.php
+++ index.php
@@ -8,6 +8,18 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo ucwords(str_replace('_','',$page)) ?> | Simple Online Groceries Ordering System</title>
+    <link rel="stylesheet" href="Font-Awesome-master/css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="./select2/css/select2.min.css">
+    <link rel="stylesheet" href="./css/custom.css">
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="./select2/js/select2.min.js"></script>
+    <link rel="stylesheet" href="DataTables/datatables.min.css">
+    <script src="DataTables/datatables.min.js"></script>
+    <script src="Font-Awesome-master/js/all.min.js"></script>
+    <script src="js/script.js"></script>
 </head>
 <body>
     
